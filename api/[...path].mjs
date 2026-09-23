5295540caecfd24db6adef29120e9311fad30ea8
Faadil1 <boussarifaadil@yahoo.com>
2026-09-23 12:57:28 -0700

    deploy: expose KEYS API through Vercel catch-all

---
+++ api/[...path].mjs
@@ -0,0 +1,16 @@
+import {
+  handleKeysVercelRequest,
+  vercelApiPathFromQuery
+} from '../../src/vercel-adapter.mjs';
+
+export const config = {
+  maxDuration: 10
+};
+
+export default async function handler(req, res) {
+  return handleKeysVercelRequest({
+    req,
+    res,
+    path: vercelApiPathFromQuery(req.query)
+  });
+}
